7281f6aa65a1b360512f0f5ed3dc1a614f232755
Mitch Seaman <mitchel.seaman@gmail.com>
2013-08-30 22:49:42 -0700

    omniauth set

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ServiceReferral::Application.routes.draw do
 
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+                     controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
   root to: "home#index"
   

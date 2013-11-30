653e946651e655c24ca28e3968bb42e95d15e018
Mitch Seaman <mitchel.seaman@gmail.com>
2013-11-29 16:36:19 -0800

    added index html dummy text

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Project.create
+user = User.create(
+	email: "mitch@onprem.com")
+
+Project.create(
+	title: "TVE Assessment",
+	client: "Discovery Communications",
+	caption: "Sourced the world's best TVE vendors for a broadcaster",
+	description: "OnPrem worked with a major US broadcaster to build out a five-year TV Everywhere roadmap")
+
+10.times do 
+	Project.create(
+		title: Faker::Company.bs.capitalize,
+		client: Faker::Company.name,
+		caption: Faker::Lorem.sentence(rand(3 + 6)),
+		description: Faker::Lorem.paragraph)
+end

bf0038a4ee0a4b3dcc1d3069a1734613f4b3c171
Mitch Seaman <mitchel.seaman@gmail.com>
2013-12-19 08:12:52 -0800

    updated home page layout

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(:version => 20131126002002) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
     t.string   "provider"
     t.string   "uid"
   end

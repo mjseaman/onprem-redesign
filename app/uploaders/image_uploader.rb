6ecb5c6e1968a55a4021cefabd574024fabda89b
Mitch Seaman <mitchel.seaman@gmail.com>
2013-12-29 22:20:03 -0800

    bootstrap and navbar updates

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   version :thumb do
-    process :resize_to_limit => [200, 200]
+    process :resize_to_limit => [240, 240]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.

b6fdf4f5e4ac8097b95005ae736a8fd16719f696
Mitch Seaman <mitchel.seaman@gmail.com>
2014-02-05 16:42:42 -0500

    added only work and team to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ OnPrem::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile << '*.js'
-  config.assets.precompile << '*.css.scss'
+  config.assets.precompile << 'team.js'
+  config.assets.precompile << 'work.js'
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

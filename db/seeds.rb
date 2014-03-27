7557fda3f0571ff976e012cc3fa8f6052819bb04
Mitch Seaman <mitchel.seaman@gmail.com>
2014-03-27 08:18:33 -0700

    un-commented seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,30 +11,40 @@
 # 	email: "mitch@onprem.com")
 # p "User created"
 
-# # Reset/create industries
-# Industry.delete_all
-# Industry.create(name: "Broadcast")
-# Industry.create(name: "Film")
-# Industry.create(name: "Music")
-# Industry.create(name: "Nonprofit")
-# Industry.create(name: "Other")
-# p "Industries set to defaults"
+# Reset/create industries
+Industry.delete_all
+Industry.create(name: "Broadcast")
+Industry.create(name: "Film")
+Industry.create(name: "Music")
+Industry.create(name: "Nonprofit")
+Industry.create(name: "Other")
+p "Industries set to defaults"
 
-# # Reset/create projects
-# Project.delete_all
-# 20.times do 
-# 	Project.create(
-# 		title: Faker::Company.bs.capitalize,
-# 		client: Faker::Company.name,
-# 		caption: Faker::Lorem.sentence(rand(3 + 6)),
-# 		description: Faker::Lorem.paragraph)
-# end
+Page.delete_all
+Page.create(name:"index",description:"/")
+Page.create(name:"about",description:"/about")
+Page.create(name:"offerings",description:"/offerings")
+Page.create(name:"work",description:"/work")
+Page.create(name:"team",description:"/team")
+Page.create(name:"careers",description:"/careers")
+Page.create(name:"innovation",description:"/innovation")
+Page.create(name:"contact",description:"/contact")
 
-# Project.all.each do |project|
-# 	project.industry = Industry.all.sample
-# 	project.save
-# end
-# p "Projects set to defaults"
+# Reset/create projects
+Project.delete_all
+20.times do 
+	Project.create(
+		title: Faker::Company.bs.capitalize,
+		client: Faker::Company.name,
+		caption: Faker::Lorem.sentence(rand(3 + 6)),
+		description: Faker::Lorem.paragraph)
+end
+
+Project.all.each do |project|
+	project.industry = Industry.all.sample
+	project.save
+end
+p "Projects set to defaults"
 
 # Reset/create people
 Person.delete_all
@@ -135,33 +145,33 @@ p "People set to defaults"
 
 
 
-# # Reset/create slides
-# Slide.delete_all
-# Slide.create(
-#   header: "We Work Hard.",
-#   quote: "What do you mean 'sleep?'",
-#   author: "Christophe Ponsart",
-#   author_title: "Founding Partner")
-# Slide.create(
-#   header: "We Play Hard.",
-#   quote: "I love these people so much.",
-#   author: "David Liu",
-#   author_title: "Senior Manager")
-# Slide.create(
-#   header: "We Build a Reputation.",
-#   quote: "I don't see how we would ever hire anyone else.",
-#   author: "Bill Client",
-#   author_title: "CTO, Megacorp")
-# Slide.create(
-#   header: "We Innovate.",
-#   quote: "It's like they had a conversation with me in 5 years and relayed the message.",
-#   author: "David Allen",
-#   author_title: "VP, Disnay")
-# Slide.create(
-#   header: "We Know Media.",
-#   quote: "This is the team building the future of entertainment.",
-#   author: "Margi Clarke",
-#   author_title: "TV Personality")
-# p "Projects set to defaults"
+# Reset/create slides
+Slide.delete_all
+Slide.create(
+  header: "We Work Hard.",
+  quote: "What do you mean 'sleep?'",
+  author: "Christophe Ponsart",
+  author_title: "Founding Partner")
+Slide.create(
+  header: "We Play Hard.",
+  quote: "I love these people so much.",
+  author: "David Liu",
+  author_title: "Senior Manager")
+Slide.create(
+  header: "We Build a Reputation.",
+  quote: "I don't see how we would ever hire anyone else.",
+  author: "Bill Client",
+  author_title: "CTO, Megacorp")
+Slide.create(
+  header: "We Innovate.",
+  quote: "It's like they had a conversation with me in 5 years and relayed the message.",
+  author: "David Allen",
+  author_title: "VP, Disnay")
+Slide.create(
+  header: "We Know Media.",
+  quote: "This is the team building the future of entertainment.",
+  author: "Margi Clarke",
+  author_title: "TV Personality")
+p "Projects set to defaults"
 
 p "Seed complete."

5ce5948978a9deb10c648fa49249501ceb3d6c4e
soloyolk <soloyolk@gmail.com>
2014-03-31 02:42:25 -0700

    forgot to add custom.js and offerings.js with last commit

---
+++ app/assets/javascripts/offerings.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  
+  var $container = $('.offering-container');
+  
+  // hover slide and fade effect (dave)
+  $('.offering').hover(function(){
+    $(this).find('.offering-detail').slideDown('fast');
+    $('img',this).stop().animate({'opacity': '.4'}, 'fast');
+    $(this).height('auto');
+  }, function(){
+    $(this).find('.offering-detail').slideUp('fast');
+    $('img',this).stop().animate({'opacity': '1'}, 'fast');
+    $(this).height('450px');
+  });
+  
+  // add randomish size classes
+  $container.find('.offering').each(function(){
+    var $this = $(this),
+        number = parseInt( $this.find('.number').text(), 10 );
+    if ( number % 7 % 2 === 1 ) {
+      $this.addClass('width2');
+    }
+    if ( number % 3 === 0 ) {
+      $this.addClass('height2');
+    }
+  });
+  
+  $container.isotope({
+    itemSelector : '.offering',
+    masonry : {
+    }
+  });
+});

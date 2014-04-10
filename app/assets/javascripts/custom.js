077ebe4cf88a8f8bf637d6e1e8d9c2a579d0a8b7
Mitch Seaman <mitchel.seaman@gmail.com>
2014-04-10 08:37:53 -0700

    fixed a typo

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 	
-	$("body").css("display", "none");
+// 	$("body").css("display", "none");
 
-    $("body").fadeIn(500);
+//     $("body").fadeIn(500);
     
-	$("a.transition").click(function(event){
-		event.preventDefault();
-		linkLocation = this.href;
-		$("body").fadeOut(500, redirectPage);		
-	});
+// 	$("a.transition").click(function(event){
+// 		event.preventDefault();
+// 		linkLocation = this.href;
+// 		$("body").fadeOut(500, redirectPage);		
+// 	});
 	
 		
-	function redirectPage() {
-		window.location = linkLocation;
-	}
+// 	function redirectPage() {
+// 		window.location = linkLocation;
+// 	}
 	
-});
+// });

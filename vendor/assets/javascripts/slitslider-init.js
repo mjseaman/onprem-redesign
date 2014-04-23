7416ee150f4b12cf418df7d065e2b46e29af042d
Mitch Seaman <mitchel.seaman@gmail.com>
2014-04-22 18:09:59 -0700

    changed slider interval

--- vendor/assets/javascripts/slitslider-init.js
+++ vendor/assets/javascripts/slitslider-init.js
@@ -2,6 +2,7 @@ $(function() {
 
   var Page = (function() {
 
+    var animateSlides = true; // When true, slides will animate. When false, they will not.
     var $navArrows = $( '#nav-arrows' ),
     $nav = $( '#nav-dots > span' ),
       slitslider = $( '#slider' ).slitslider( {
@@ -20,25 +21,35 @@ $(function() {
       },
       initEvents = function() {
 
-        // add navigation events
+        // Navigation events
+        // When the user clicks the "last" button, go to the previous slide
+        
         $navArrows.children( ':last' ).on( 'click', function() {
 
+                animateSlides = false; // Stop animation
                 slitslider.next();
                 return false;
 
         } );
 
+        // When the user clicks the "next" button, go to the next slide
+        
         $navArrows.children( ':first' ).on( 'click', function() {
                 
+                animateSlides = false; // Stop animation
                 slitslider.previous();
                 return false;
 
         } );
 
+        // When the user clicks a circle navigation button, go to the corresponding slide
+        
         $nav.each( function( i ) {
         
           $( this ).on( 'click', function( event ) {
             
+            animateSlides = false; // Stop animation
+            console.log(animateSlides);
             var $dot = $( this );
             
             if( !slitslider.isActive() ) {
@@ -53,12 +64,19 @@ $(function() {
           
           } );
 
+          // Move to the next slide at a 4-second interval
+
+          // Interval at which the 
+          var sliderInterval = 4000;
+
           function animate() {
-            slitslider.next();
-            setTimeout(animate, 5000);
+            if (animateSlides == true) {
+              slitslider.next();
+              setTimeout(animate, sliderInterval);
+            }
           }
 
-          setTimeout(animate, 5000);
+          setTimeout(animate, sliderInterval);
           
         } );
 
